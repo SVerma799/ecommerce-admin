7f12ab6d71d0894157e34d85dde089cdee392521
Shubham Verma <49226983+SVerma799@users.noreply.github.com>
2023-06-16 16:52:47 -0700

    Changes done to prevent Hydration error

--- src/pages/admin/Settings.tsx
+++ src/pages/admin/Settings.tsx
@@ -1,11 +1,9 @@
-"use client";
-
 import Navbar from "@/components/Navbar";
 import { SelectField } from "@/components/form/Select";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useRouter } from "next/router";
-import { FC, useEffect } from "react";
-import { useTranslation } from "react-i18next";
+import { FC, useEffect, useState } from "react";
+import { useTranslation } from "next-i18next";
 import { useTheme } from "next-themes";
 
 const Settings: FC = () => {
@@ -14,6 +12,7 @@ const Settings: FC = () => {
   const { t } = useTranslation("common");
   const { push, pathname } = useRouter();
   const { theme, setTheme } = useTheme();
+  const [persistedlocale, setPersistedLocale] = useState("en");
 
   useEffect(() => {
     let persistedTheme = localStorage.getItem("theme") ?? "light";
@@ -21,21 +20,26 @@ const Settings: FC = () => {
       persistedTheme = "light";
     }
     setTheme(persistedTheme);
-  }, [setTheme]);
+    setPersistedLocale(localStorage.getItem("locale") ?? "en");
+  }, [setTheme, setPersistedLocale]);
 
   return (
     <div className="flex gap-2">
       <Navbar />
-      <div className="w-full">
+      <div className="w-1/2 p-7">
         <h1 className="text-3xl">{t("Settings")}</h1>
-        <div id="Language m-8 border-b border-b-indigo-800">
+
+        <div className=" flex flex-col border border-indigo-800 m-7 p-7 gap-4 rounded">
           <SelectField
-            label="Language"
-            labelClassName=" text-xl text-slate-950 dark:text-white"
-            className="px-4 py-2 my-1  rounded-md w-full text-slate-950 border-2 dark:text-white  border-indigo-800 dark:border-none"
+            label={t("Languages")}
+            labelClassName=" text-xl text-slate-950 dark:text-white mb-2"
+            className="px-4 py-2 my-1  rounded-md text-slate-950 border-2 dark:text-white  border-indigo-800 dark:border-none w-1/2 "
             onChange={(e: any) => {
+              setPersistedLocale(e.target.value);
               push(pathname, undefined, { locale: e.target.value });
+              localStorage.setItem("locale", e.target.value);
             }}
+            value={persistedlocale}
           >
             {locales &&
               locales.map((locale) => (
@@ -44,23 +48,23 @@ const Settings: FC = () => {
                 </option>
               ))}
           </SelectField>
+          <SelectField
+            label={t("Themes")}
+            labelClassName=" text-xl text-slate-950 dark:text-white  mb-2"
+            className="px-4 py-2 my-1  rounded-md w-1/2 text-slate-950 border-2 dark:text-white  border-indigo-800 dark:border-none"
+            onChange={(e: any) => {
+              setTheme(e.target.value);
+            }}
+            value={theme}
+          >
+            {themes &&
+              themes.map((th) => (
+                <option key={th} value={th}>
+                  {th}
+                </option>
+              ))}
+          </SelectField>
         </div>
-        <SelectField
-          label="Theme"
-          labelClassName=" text-xl text-slate-950 dark:text-white"
-          className="px-4 py-2 my-1  rounded-md w-full text-slate-950 border-2 dark:text-white  border-indigo-800 dark:border-none"
-          onChange={(e: any) => {
-            setTheme(e.target.value);
-          }}
-          value={theme}
-        >
-          {themes &&
-            themes.map((th) => (
-              <option key={th} value={th}>
-                {th}
-              </option>
-            ))}
-        </SelectField>
       </div>
     </div>
   );
