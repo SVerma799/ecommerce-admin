cd5e4aa28d37ac3d1c716d94b765fc7a251dc757
Shubham Verma <49226983+SVerma799@users.noreply.github.com>
2023-06-16 17:43:20 -0700

    Changes done to add the Add Product Page

---
+++ src/components/admin/Products/AddProduct.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+const AddProduct: FC = () => <div className="mt-8">AddProduct</div>;
+
+export default AddProduct;
--- src/pages/admin/Products.tsx
+++ src/pages/admin/Products.tsx
@@ -1,10 +1,38 @@
+import { FC, useState } from "react";
 import Navbar from "@/components/Navbar";
+import { Button } from "@/components/form/button";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import AddProduct from "@/components/admin/Products/AddProduct";
 
-export default function Products() {
+const Products: FC = () => {
+  const { t } = useTranslation("common");
+  const [showAddProduct, setShowAddProduct] = useState<boolean>(false);
   return (
     <div className="flex gap-2">
       <Navbar />
-      <h1>Products</h1>
+      <div className="w-full flex flex-col p-7">
+        <h1 className="text-3xl">{t("Products")}</h1>
+        <Button
+          buttonClass="w-[10%] mt-5"
+          onClick={() => setShowAddProduct(true)}
+        >
+          {t("Add_Products")}
+        </Button>
+
+        {/* ********************************** Add Product Comp *********************** */}
+        {showAddProduct && <AddProduct />}
+        {/* ********************************** Add Product Comp *********************** */}
+      </div>
     </div>
   );
+};
+export default Products;
+
+export async function getStaticProps({ locale }: any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
 }
